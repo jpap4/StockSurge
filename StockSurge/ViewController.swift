f81d6734f7380b496446005e4fcb41cb2c0044fc
John Pappas <johncpappas4@gmail.com>
2020-12-15 17:28:20 -0500

    Initial Commit

---
+++ StockSurge/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  StockSurge
+//
+//  Created by John Pappas on 12/15/20.
+//  Copyright © 2020 John Pappas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
